5902283e97f066ead316c1453843b555785fac71
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-07-14 12:44:10 +0600

    clock run and state or lifestyle

--- myreact/src/components/Clock.js
+++ myreact/src/components/Clock.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 class Clock extends React.Component {
+    state = { date: new Date() };
+
+    componentDidMount() {
+        this.clockTimer = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clockTimer);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date(),
+        });
+    }
+
     render() {
-        return (
-            <h1 className="text">
-                {new Date().toLocaleTimeString(this.props.locale)}
-            </h1>
-        );
+        return <h1 className="text">{this.state.date.toLocaleTimeString(this.props.locale)}</h1>;
     }
 }
 export default Clock;
