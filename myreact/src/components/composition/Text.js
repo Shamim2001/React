2fa57ed94f8598b0ff0233d583a52f721308fbae
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-08-08 12:18:51 +0600

    inheritance & composition

---
+++ myreact/src/components/composition/Text.js
@@ -0,0 +1,12 @@
+export default function Text({ addEmoji, addBracket }) {
+    let text = 'I am JavaScript Language';
+
+    if (addEmoji) {
+        text = addEmoji(text, '🧡');
+    }
+    if (addBracket) {
+        text = addBracket(text);
+    }
+
+    return <div>{text}</div>;
+}
